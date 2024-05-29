84ad8b1d6132285e7cfdf8338e76690dc96185bf
Aaron Dewes <aaron.dewes@protonmail.com>
2024-05-29 08:56:10 +0200

    Update kopium

--- codegen.py
+++ codegen.py
@@ -30,10 +30,6 @@ crds = yaml.safe_load_all(
 for crd in crds:
     file_name = crd["metadata"]["name"].removesuffix(".traefik.io")
     rust_code = ""
-    if file_name == "middlewares":
-        del crd["spec"]["versions"][0]["schema"]["openAPIV3Schema"]["properties"][
-            "spec"
-        ]["properties"]["plugin"]
     # Save the CRD as a tmp yaml file
     with tempfile.NamedTemporaryFile(mode="w") as f:
         yaml.dump(crd, f)
@@ -48,6 +44,7 @@ for crd in crds:
                 "-b",
                 "--derive=Default",
                 "--derive=PartialEq",
+                "--smart-derive-elision",
             ],
             capture_output=True,
         )
@@ -57,8 +54,8 @@ for crd in crds:
         rust_code = rust_code.stdout.decode("utf-8")
 
     rust_code = rust_code.replace(
-        f"// kopium command: kopium -f {tmp_file} --schema=derived --docs -b --derive=Default --derive=PartialEq",
-        f"// kopium command: kopium -f {file_name}.yml --schema=derived --docs -b --derive=Default --derive=PartialEq",
+        f"// kopium command: kopium -f {tmp_file} --schema=derived --docs -b --derive=Default --derive=PartialEq --smart-derive-elision",
+        f"// kopium command: kopium -f {file_name}.yml --schema=derived --docs -b --derive=Default --derive=PartialEq --smart-derive-elision",
     )
     rust_code = "\n".join(
         [
